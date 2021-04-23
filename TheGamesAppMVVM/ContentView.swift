faf6db9e29848558e31826338b77428b01e22db8
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2021-04-23 10:50:27 +0700

    Initial Commit

---
+++ TheGamesAppMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheGamesAppMVVM
+//
+//  Created by Reza Ramadhan Irianto on 23/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
